1fff9d83e3532461efee8ee93ea4457de804e61c
ujangpudin04 <ujangpudin04@gmail.com>
2024-11-16 16:06:40 +0700

    start create

---
+++ src/app/[lang]/(dashboard)/(private)/tambah/page.jsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+
+const CreateForm = () => {
+  return (
+    <Grid container xs>
+      <Grid Item xs>
+        Create
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CreateForm;
